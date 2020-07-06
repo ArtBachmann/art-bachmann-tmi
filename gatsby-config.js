9ce923412d9f1a9ea47787accfee81ec02b93ea0
Art Bachmann <bachmann.art@gmail.com>
2020-07-06 12:41:39 +0300

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,7 +22,7 @@ module.exports = {
       options: {
         url:
           process.env.WPGRAPHQL_URL ||
-          `https://dev-gatsby-source-wordpress-v4.pantheonsite.io/graphql`,
+          `http://test.artbachmann.fi/art-bachmann-tmi/graphql`,
         verbose: true,
         develop: {
           hardCacheMediaFiles: true,
@@ -37,15 +37,16 @@ module.exports = {
             limit:
               process.env.NODE_ENV === `development`
                 ? // Lets just pull 50 posts in development to make it easy on ourselves.
-                  50
+                50
                 : // and we don't actually need more than 5000 in production for this particular site
-                  5000,
+                5000,
           },
         },
       },
     },
     `gatsby-plugin-chakra-ui`,
     `gatsby-transformer-sharp`,
+    `gatsby-plugin-material-ui`,
     {
       resolve: "gatsby-plugin-react-svg",
       options: {
